a5ffb712e9aa484b3a4c4a327b610d67838d1cb1
Drive <kr.sannarong@gmail.com>
2020-11-05 19:34:26 +0700

    [Update] UI

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,14 +11,7 @@
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App",
-    };
-  },
-};
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
